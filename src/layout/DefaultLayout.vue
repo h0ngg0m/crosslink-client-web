fd6bdf7175108183899d5493ebaf16fd6e227544
honggom <jaehongbyeon98@gmail.com>
2023-12-26 19:03:21 +0900

    :sparkles: 레이아웃, 폰트, 기타세팅

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout>
+    <v-app-bar flat color="primary">
+      <v-app-bar-title text="CrossLink"></v-app-bar-title>
+      <template #append>
+        <v-tooltip v-if="login" text="마이 페이지" location="bottom">
+          <template #activator="{ props }">
+            <v-btn v-bind="props" icon="mdi-account"></v-btn>
+          </template>
+        </v-tooltip>
+        <v-tooltip v-if="login" text="로그아웃" location="bottom">
+          <template #activator="{ props }">
+            <v-btn v-bind="props" icon="mdi-logout"></v-btn>
+          </template>
+        </v-tooltip>
+        <v-tooltip v-else text="로그인" location="bottom">
+          <template #activator="{ props }">
+            <v-btn v-bind="props" icon="mdi-login"></v-btn>
+          </template>
+        </v-tooltip>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer border="none"> </v-navigation-drawer>
+
+    <v-navigation-drawer location="right" border="none"> </v-navigation-drawer>
+
+    <v-main>
+      <slot />
+    </v-main>
+
+    <v-footer app name="footer">FOOTER</v-footer>
+  </v-layout>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const login = ref(true)
+</script>
