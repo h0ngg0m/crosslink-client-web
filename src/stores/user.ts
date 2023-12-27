32ebbf518963853aa0cf3a1f45f0f45a314d30af
honggom <jaehongbyeon98@gmail.com>
2023-12-28 00:14:49 +0900

    :bug: 새로고침 시 상태가 유지되지 않는 문제 수정

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,23 +3,25 @@ import { routerPush } from '@/utils/common'
 import { computed, ref } from 'vue'
 import { isTokenExpired } from '@/utils/token'
 
-export const useUserStore = defineStore('user', () => {
-  const accessToken = ref<string | null>(null)
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const accessToken = ref<string | null>(null)
 
-  const loginFlag = computed<boolean>(() => {
-    return !isTokenExpired(accessToken.value)
-  })
+    const loginFlag = computed<boolean>(() => {
+      return !isTokenExpired(accessToken.value)
+    })
 
-  function saveAccessToken(token: string): void {
-    localStorage.setItem('accessToken', token)
-    accessToken.value = token
-  }
+    function saveAccessToken(token: string): void {
+      accessToken.value = token
+    }
 
-  async function logout(): Promise<void> {
-    localStorage.removeItem('accessToken')
-    accessToken.value = null
-    await routerPush('/')
-  }
+    async function logout(): Promise<void> {
+      accessToken.value = null
+      await routerPush('/')
+    }
 
-  return { loginFlag, saveAccessToken, logout }
-})
+    return { accessToken, loginFlag, saveAccessToken, logout }
+  },
+  { persist: true }
+)
